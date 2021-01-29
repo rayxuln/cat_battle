773e2f3d7da3aceb47f74cde343fc4234d39f915
Raiix <imbosslx@sina.com>
2021-01-29 18:21:25 +0800

    6 - Basic client server framework

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -2,15 +2,15 @@ extends Node
 
 
 onready var world:Node = null
-onready var player_name_label:Label
-onready var player_heart_group
-onready var play_mouse_count_label:Label
-onready var summary_panel:Panel
+onready var player_name_label:Label = $UILayer/Control/HUD/Group1/PlayerNameLabel
+onready var player_heart_group = $UILayer/Control/HUD/Group1/HeartGroup
+onready var play_mouse_count_label:Label = $UILayer/Control/HUD/Group2/MouseCountLabel
+onready var summary_panel:Panel = $UILayer/Control/SummaryPanel
 onready var chat_display = $UILayer/Control/ChatDipaly
-onready var pause_panel:Panel
+onready var pause_panel:Panel = $UILayer/Control/PausePanel
 onready var main_menu:Control = $UILayer/Control/MainMenu
-onready var create_host_dialog:AcceptDialog
-onready var connect_to_host_dialog:AcceptDialog
+onready var create_host_dialog:AcceptDialog = $UILayer/Control/CreateHostDialog
+onready var connect_to_host_dialog:AcceptDialog = $UILayer/Control/ConnectToHostDialog
 onready var screen_touch_ui:Control
 onready var sound_player:AudioStreamPlayer
 
@@ -20,6 +20,9 @@ func _ready():
 	hide_hud()
 	hide_panels()
 
+func _process(delta):
+	if GameSystem.is_game_started() and Input.is_action_just_pressed("ui_cancel"):
+		pause_panel.visible = not pause_panel.visible
 #----- Methods -----
 func hide_ui():
 	main_menu.visible = false
@@ -28,15 +31,53 @@ func show_ui():
 	main_menu.visible = true
 
 func hide_hud():
-	pass
+	$UILayer/Control/HUD.visible = false
+	
 	
 func show_hud():
-	pass
+	$UILayer/Control/HUD.visible = true
+	
+func show_create_host_dialog():
+	create_host_dialog.port.text = str(GameSystem.default_port)
+	create_host_dialog.player_name.text = "123"
+	create_host_dialog.popup_centered()
+
+func show_connect_to_host_dialog():
+	connect_to_host_dialog.player_name.text = "321"
+	connect_to_host_dialog.popup_centered()
 
 func hide_panels():
-	pass
+	summary_panel.visible = false
+	pause_panel.visible = false
 	
 
 #----- Signals -----
 func _on_ChatDipaly_command_entered(cmd):
 	GameSystem.send_cmd(cmd)
+
+
+func _on_ServerButton_pressed():
+	show_create_host_dialog()
+
+func _on_ClientButton_pressed():
+	show_connect_to_host_dialog()
+
+func _on_QuitButton_pressed():
+	get_tree().quit(0)
+
+
+func _on_CreateHostDialog_confirmed():
+	GameSystem.start_as_server(create_host_dialog.player_name.text, create_host_dialog.get_port())
+	
+
+func _on_ConnectToHostDialog_confirmed():
+	GameSystem.start_as_client(connect_to_host_dialog.player_name.text, connect_to_host_dialog.get_address(), connect_to_host_dialog.get_port())
+	
+	
+
+func _on_PausePanel_go_to_main_menu():
+	GameSystem.stop_game()
+	GameSystem.stop_network()
+	
+	show_ui()
+	hide_hud()
--- scripts/GameSystem.gd
+++ scripts/GameSystem.gd
@@ -23,6 +23,70 @@ func _ready():
 	add_child(game_console)
 
 #----- Methods -----
+func start_as_server(p_name, port:int):
+	if network_manager != null:
+		stop_network()
+		
+	player_name = p_name
+	
+	network_manager = NetworkManagerServer.new()
+	network_manager.port = port
+	network_manager.name = "NetworkManagerServer"
+	
+	network_manager.connect("server_started", self, "_on_server_started")
+	network_manager.connect("server_fail", self, "_on_server_fail")
+	
+	add_child(network_manager)
+
+func start_as_client(p_name, address:String, port:int):
+	if network_manager != null:
+		stop_network()
+	
+	player_name = p_name
+	
+	send_msg("正在连接服务器(%s:%d)..." % [address, port])
+	network_manager = NetworkManagerClient.new()
+	network_manager.server_address = address
+	network_manager.port = port
+	network_manager.name = "NetworkManagerClient"
+	
+	network_manager.connect("connected_to_server", self, "_on_connected_to_server")
+	network_manager.connect("connection_failed", self, "_on_connection_failed")
+	network_manager.connect("server_disconnected", self, "_on_server_disconnected")
+	
+	add_child(network_manager)
+
+func load_world():
+	pass
+
+func start_game():
+	load_world()
+	
+	game_started = true
+	emit_signal("game_start")
+
+func stop_game():
+	game_started = false
+	emit_signal("game_stop")
+	
+	if game_manager.world:
+		game_manager.world.queue_free()
+		game_manager.world = null
+		
+func is_game_started():
+	return game_started
+
+func stop_network():
+	network_manager.queue_free()
+	network_manager = null
+	
+
+func set_main_player_manager(p):
+	main_player_manger = p
+
+func add_server_side_player():
+	pass
+
 func set_game_manager(gm):
 	game_manager = gm
 	game_console.console_ui = game_manager.chat_display
@@ -41,3 +105,47 @@ func send_boardcast(s):
 	
 func send_cmd(s, sender=null):
 	game_console.send_cmd(s, sender)
+#----- RPCs -----
+#----- Signals -----
+func _on_connected_to_server():
+	game_manager.hide_ui()
+	game_manager.show_hud()
+	start_game()
+	send_msg("连接到服务器成功！")
+
+func _on_connection_failed():
+	game_manager.show_ui()
+	send_msg("连接到服务器失败！")
+	
+	stop_network()
+
+func _on_server_disconnected():
+	game_manager.show_ui()
+	game_manager.hide_hud()
+	game_manager.hide_panels()
+	
+	stop_game()
+	send_msg("与服务器断开连接！")
+
+	stop_network()
+
+func _on_server_fail():
+	stop_game()
+	send_msg("创建服务器失败！")
+	game_manager.show_ui()
+	
+	stop_network()
+
+func _on_server_started():
+	send_msg("创建服务器(端口：%d)成功！" % network_manager.port)
+	game_manager.hide_ui()
+	game_manager.show_hud()
+	
+	start_game()
+	
+	add_server_side_player()
+	
+	send_boardcast("> 等待其他玩家加入...")
+
+
+
